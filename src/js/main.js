e6925bc9a79af42e15d50a9b8200a2aff04d2ab4
Mikhail326 <misha.stelmakh1995@gmail.com>
2023-10-14 12:32:52 +0300

    fix bag bg

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 const menuBtn = document.querySelector('.header__mobile-btn')
 const menu = document.querySelector('.header__menu')
 const menuRight = document.querySelector('.header__right')
+const header = document.querySelector('.header')
 
 const handlerMenu = () => {
   menuBtn.classList.toggle('active-btn')
   menu.classList.toggle('active-menu')
   menuRight.classList.toggle('active-menu')
+  header.classList.toggle('header-active')
 }
 
 menuBtn.addEventListener('click', handlerMenu)
